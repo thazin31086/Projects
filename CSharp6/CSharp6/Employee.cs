218c155fcb652410da4524e78d1f917c84d0f2bc
Jasmine Aung <thazin31086@gmail.com>
2017-01-09 13:52:30 +1100

    Class Exercise

---
+++ CSharp6/CSharp6/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp6
+{
+    class Employee
+    {
+    }
+}
+/*
+Class 
+=====
+class is a reference type, 
+an instance of a class is called object 
+
+Method
+====== 
+is a chunk of code that does sth(modify object, cal, )
+it can take information using parameters 
+it can return a value
+*/
