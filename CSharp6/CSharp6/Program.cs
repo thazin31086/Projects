9696f27e980770cf1618c017287bf06d846155f8
Jasmine Aung <thazin31086@gmail.com>
2017-01-09 13:25:31 +1100

    for, While and Do while loop

--- CSharp6/CSharp6/Program.cs
+++ CSharp6/CSharp6/Program.cs
@@ -11,10 +11,29 @@ namespace CSharp6
             Console.WriteLine($"Hello Application {name}");
 
             //String Interpolation example with loop variable 
+            //for will loop over a know squence
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine($"Hello Loop {i.ToString()}");
             }
+
+            //While loop run until cerain condition is true
+            int j = 0;
+            while (j < 0)
+            {
+                Console.WriteLine($"Hello Loop {j.ToString()}");
+                j++;
+
+            }
+            
+            //Do While loop run always run once
+            do
+            {
+                Console.WriteLine($"Do- While ::Hello Loop {j.ToString()}");
+                j++;
+
+            } while (j < 0);
+
             Console.ReadLine();
         }
     }
