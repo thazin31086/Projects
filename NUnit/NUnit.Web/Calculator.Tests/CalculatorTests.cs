ddc7f9c9ef13f9c60a61fc5aba04f68e77094fe0
Jasmine Aung <thazin31086@gmail.com>
2017-01-23 14:07:27 +1100

    NUnit Adapter Installation In Progress

---
+++ NUnit/NUnit.Web/Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.Add(1, 3);
+
+            Assert.That(result, Is.EqualTo(3)); 
+        }
+
+
+        [Test]
+        public void ShouldMultiplyTwoNumbers()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.Multiply(2, 10);
+
+            Assert.That(result, Is.EqualTo(20));
+        }
+    }
+}
---
+++ NUnit/NUnit.Web/Calculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculator
+{
+    public class SimpleCalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            //bug for demo purpose
+            return a + b;
+        }
+    }
+}
