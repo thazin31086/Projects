e9438bdbaaf3b037208c552a5836f0248742d587
Jasmine Aung <thazin31086@gmail.com>
2017-03-02 15:54:22 +1100

    In - Progess Movie Review Angular 2 Web Site Added

--- Angular2/FirstAngular2/app/app.component.ts
+++ Angular2/FirstAngular2/app/app.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
+    styles:  ['styles/core.less'],
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
-    `
+    <div>
+        <nav class='navbar navbar-default'>
+            <div class='container-fluid'>
+                <a class='navbar-brand' [routerLink]="['/welcome']">{{pageTitle}}</a>
+                <ul class='nav navbar-nav'>
+                    <li><a [routerLink]="['/welcome']">Home</a></li>
+                    <li><a [routerLink]="['/products']">Movie List</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class='container'>
+            <router-outlet></router-outlet>
+        </div>
+     </div>
+     `
 })
-export class AppComponent { }
+export class AppComponent {
+    pageTitle: string = 'Movie Review';
+}
--- Angular2/FirstAngular2/readme.md
+++ Angular2/FirstAngular2/readme.md
@@ -1,24 +1,8 @@
-Angular 2 
-==========
-
-Setting up Angular 2 Environment
-=================================
-http://www.angular.io 
-https://github.com/angular/quickstart
-https://github.com/angular/angular-cli
-https://github.com/DeborahK/Angular2-GettingStarted
-
-npm - Node Package Manager 
---------------------------
-
 1) Open a command prompt in the project's root directory (APM)
-
 
 2) Type: `npm install`
     This installs the dependencies as defined in the package.json file.
     
-
 3) Type: `npm start`
     This launches the TypeScript compiler (tsc) to compile the application and wait for changes. 
-  
-   It also starts the lite-server and launches the browser to run the application.
+    It also starts the lite-server and launches the browser to run the application.
